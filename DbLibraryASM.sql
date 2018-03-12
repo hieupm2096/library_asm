5128a689e40dce88b671941817a7c2ac95d3b00f
hieupm2096 <hieupm2096@users.noreply.github.com>
2018-03-12 19:38:46 +0700

    edit database

--- DbLibraryASM.sql
+++ DbLibraryASM.sql
@@ -20,8 +20,9 @@ CREATE TABLE BookHistory(
     CONSTRAINT fk_bookhistory_book_code FOREIGN KEY (_code) REFERENCES Book(_code),
     _reader_id VARCHAR(5),
     CONSTRAINT fk_bookhistory_reader_id FOREIGN KEY (_reader_id) REFERENCES Reader(_id),
-    _borrowedDate DATETIME NOT NULL,
-    _returnedDate DATETIME
+    _borrowed_date DATETIME NOT NULL,
+    _expire_date DATETIME NOT NULL,
+    _returned_date DATETIME
 )
 
 INSERT INTO Book(_code, _name, _author, _status) 
@@ -29,6 +30,6 @@ INSERT INTO Book(_code, _name, _author, _status)
             ('B0002', 'Book 2', 'Author 2', 1),
             ('B0003', 'Book 3', 'Author 3', 1)
 
-INSERT INTO BookHistory(_code, _borrowedDate, _returnedDate)
-VALUES ('B0001', '20180304', '20180307'),
-    ('B0002', '20180304', '20180307')
+INSERT INTO BookHistory(_code, _readed_id, _borrowed_date, _expire_date, _returned_date)
+VALUES ('B0001', 'R0001', '20180304', '20180307', '20180307'),
+       ('B0002', 'R0001', '20180304', '20180307', '20180307')
